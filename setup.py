4f618e3c6c3bb8d10dab70bc61d3a8d5208d3b1f
Michael Scales <mscales@mindwireless.com>
2017-01-05 16:03:57 -0600

    first files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Daily SLA Program',
+    version='3.0',
+    packages=['automated_sla_tool'],
+    author='Michael Scales',
+    author_email='mscales@mindwireless.com'
+)
