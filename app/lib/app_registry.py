5136a9d6f4302dc6d2091da911e6ac9e84128736
Michael Scales <mscales@mindwireless.com>
2017-06-02 13:20:18 -0500

    updated dockerfiles for current deployment

--- app/lib/app_registry.py
+++ app/lib/app_registry.py
@@ -22,11 +22,7 @@ class ModelRegistry(object):
 
     def get(self, url, **kwargs):
         if url not in self._registry:
-            mapped_cls = kwargs.pop('cls', None)  # get the custom mapped class
-            engine = create_engine(url, **kwargs)
-            if mapped_cls:
-                mapped_cls.metadata.create_all(engine)  # This creates the declarative base tables
-            session_factory = sessionmaker(bind=engine)
-            Session = scoped_session(session_factory)
-            self._registry[url] = Session
+            pass
+            # Model stuff
+            # self._registry[url] = model
         return self._registry[url]
